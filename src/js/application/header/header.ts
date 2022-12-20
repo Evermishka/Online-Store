c9c858d669535438230a31eb2cc9d5cdbec2f25c
pain4metoo <pain4metoo@gmail.com>
2022-12-20 20:33:34 +0100

    feat: add logo

--- src/js/application/header/header.ts
+++ src/js/application/header/header.ts
@@ -2,6 +2,9 @@ import Control from '../../common/control';
 
 export class Header extends Control {
   constructor(parentNode: HTMLElement) {
-    super(parentNode, 'header', 'header', 'header');
+    super(parentNode, 'header', 'header', '');
+    const headerInner = new Control(this.node, 'div', 'header_inner', '');
+    const headerLogo: { node: HTMLImageElement } = new Control(headerInner.node, 'img', 'header_logo', '');
+    headerLogo.node.src = './assets/svg/logo.svg';
   }
 }
