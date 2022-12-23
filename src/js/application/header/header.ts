425485f00b608d0cfb768bb53bbbd30f934f08dd
pain4metoo <pain4metoo@gmail.com>
2022-12-23 14:29:48 +0100

    fix: icon

--- src/js/application/header/header.ts
+++ src/js/application/header/header.ts
@@ -1,4 +1,6 @@
 import Control from '../../common/control';
+import logo from '../../../assets/svg/logo.svg';
+import cartMenu from '../../../assets/images/cart.png';
 
 export class Header extends Control {
   constructor(parentNode: HTMLElement) {
@@ -6,15 +8,15 @@ export class Header extends Control {
 
     const headerInner = new Control(this.node, 'div', 'header_inner', '');
     const headerLogo: { node: HTMLImageElement } = new Control(headerInner.node, 'img', 'header_logo', '');
-    headerLogo.node.src = '../../assets/svg/logo.svg';
+    headerLogo.node.src = logo;
     headerLogo.node.alt = 'image logo';
 
     const headerSumBlock = new Control(headerInner.node, 'div', 'header_inner_sum', '');
     const headerSumText = new Control(headerSumBlock.node, 'span', 'header_sum_text', 'Cart total');
     const headerSum = new Control(headerSumBlock.node, 'span', 'header_sum', '€0.00');
 
-    const headerCart: { node: HTMLImageElement } = new Control(headerInner.node, 'img', 'header_logo', '');
-    headerCart.node.src = '';
+    const headerCart: { node: HTMLImageElement } = new Control(headerInner.node, 'img', 'header_cart', '');
+    headerCart.node.src = cartMenu;
     headerCart.node.alt = 'cart';
   }
 }
