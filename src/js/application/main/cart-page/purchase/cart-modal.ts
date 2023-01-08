f3df3b3ca261d831b5afcf959abb5091bf6e7128
Evgeniya Ermishina <evermishka@yandex.ru>
2023-01-08 12:02:14 +0300

    feat; add purchase form validation

--- src/js/application/main/cart-page/purchase/cart-modal.ts
+++ src/js/application/main/cart-page/purchase/cart-modal.ts
@@ -2,18 +2,69 @@ import Control from '../../../../common/control';
 import { formData } from '../../../../data/form';
 import { FormLine } from './form-line';
 
+interface Errors {
+  [key: string]: Control<HTMLElement>;
+}
+
 export class CartModal extends Control {
+  private inputs: FormLine[];
+  private errors: Errors;  
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', 'purchase');
-    new Control(this.node, 'p', 'purchase_title', 'Personal details');
-    const formLineName = new FormLine(this.node, formData.name);
-    const formLinePhone = new FormLine(this.node, formData.phone);
-    const formLineAddress = new FormLine(this.node, formData.address);
-    const formLineEmail = new FormLine(this.node, formData.email);
-    new Control(this.node, 'p', 'purchase_title', 'Credit card details');
-    const formLineCardNumber = new FormLine(this.node, formData.cardNumber);
-    const formLineCardExpire = new FormLine(this.node, formData.cardExpire);
-    const formLineCardCvv = new FormLine(this.node, formData.cardCvv);
-    const confirmButton = new Control(this.node, 'button', 'purchase_button', 'CONFIRM')
+    this.inputs = [];
+    this.errors = {};
+    const purchaseForm = new Control(this.node, 'form', 'purchase_form');
+    new Control(purchaseForm.node, 'p', 'purchase_title', 'Personal details');
+    this.inputs.push(new FormLine(purchaseForm.node, formData.name));
+    this.inputs.push(new FormLine(purchaseForm.node, formData.phone));
+    this.inputs.push(new FormLine(purchaseForm.node, formData.address));
+    this.inputs.push(new FormLine(purchaseForm.node, formData.email));
+    new Control(purchaseForm.node, 'p', 'purchase_title', 'Credit card details');
+    this.inputs.push(new FormLine(purchaseForm.node, formData.cardNumber));
+    this.inputs.push(new FormLine(purchaseForm.node, formData.cardExpire));
+    this.inputs.push(new FormLine(purchaseForm.node, formData.cardCvv));
+    const confirmButton: { node: HTMLButtonElement } = new Control(
+      purchaseForm.node,
+      'button',
+      'purchase_button',
+      'CONFIRM'
+    );
+    confirmButton.node.type = 'button';
+    confirmButton.node.onclick = () => this.sendForm();    
+  }
+
+  private sendForm(): void {
+    if (this.checkForm()) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+  private checkForm(): boolean {
+    const results: boolean[] = this.inputs.map((el: FormLine) => this.checkInput(el, el.name));
+    return !results.includes(false);
+  }
+  private checkInput(input: FormLine, inputName: string): boolean {
+    if (this.isValidInput(input.value, formData[inputName].validation)) {
+      this.hideError(inputName);      
+      return true;
+    } else {
+      this.showError(inputName, input.node);
+      input.node.oninput = () => this.checkInput(input, inputName);      
+      return false;
+    }
+  }
+  private isValidInput(input: string, validation: RegExp): boolean {
+    return validation.test(input);
+  }
+  private showError(name: string, parentNode: HTMLElement | null): void {
+    if (!this.errors[name]) this.errors[name] = new Control(parentNode, 'span', 'purchase_error', formData[name].error);
+  }
+  private hideError(name: string): void {
+    if (this.errors[name]) {
+      this.errors[name].destroy();
+      delete this.errors[name];
+    }
   }
 }
--- src/js/application/main/cart-page/purchase/form-line.ts
+++ src/js/application/main/cart-page/purchase/form-line.ts
@@ -2,8 +2,13 @@ import Control from '../../../../common/control';
 import { PurchaseData } from '../../../../data/form';
 
 export class FormLine extends Control {
+  public value: string;
+  public name: string;
+  
   constructor(parentNode: HTMLElement, formData: PurchaseData) {
-    super(parentNode, 'div', 'purchase_line-wrapper');    
+    super(parentNode, 'div', 'purchase_line-wrapper');
+    this.name = formData.name;
+    this.value = '';
     const label: { node: HTMLLabelElement } = new Control(this.node, 'label', 'purchase_label', formData.label);
     label.node.htmlFor = formData.id;
     const name: { node: HTMLInputElement } = new Control(this.node, 'input', 'purchase_input', '');
@@ -11,5 +16,8 @@ export class FormLine extends Control {
     name.node.name = formData.name;
     name.node.placeholder = formData.placeholder;
     name.node.id = formData.id;
+    name.node.oninput = () => {
+      this.value = name.node.value;
+    };
   }
 }
